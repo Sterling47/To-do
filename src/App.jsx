3c5f403eec1bf1d22174e911c19f6ec2bf8af475
Adam <konber@gmail..com>
2023-09-15 13:48:00 -0600

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState, useEffect } from 'react';
+import apiRequest from './apiRequest';
 
 function App() {
   const API_URL = 'http://localhost:3500/items'
@@ -28,7 +29,7 @@ function App() {
       } finally {
         setIsLoading(false);
       }
-    }
+    } 
     // used setTimeout() just to simulate loading time 
     setTimeout(() => {
       fetchItems();
@@ -36,11 +37,23 @@ function App() {
 
   }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length -1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const  postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    };
+
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result)
+
   }
 
   const handleCheck = (id) => {
